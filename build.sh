8344db53a13a389e36c547f01d1c5c9b05a42e05
hotjuicew <hotjuicew@outlook.com>
2023-07-06 17:29:04 +0800

    update

---
+++ build.sh
@@ -0,0 +1,7 @@
+
+curl -L -o hugoArticleEncryptor "https://github.com/hotjuicew/hugoArticleEncryptor/releases/download/stable/hugoArticleEncryptor-linux-amd64"
+
+chmod +x ./hugoArticleEncryptor
+
+./hugoArticleEncryptor JuiceBar
+
