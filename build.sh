23199666c12e744b9e998cc6262ae7be0d35f3f4
hotjuicew <hotjuicew@outlook.com>
2023-07-07 19:05:39 +0800

    fix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -x
 
 hugo version
 
-curl -sSFL -o hugoArticleEncryptor "https://github.com/hotjuicew/hugoArticleEncryptor/releases/download/stable/hugoArticleEncryptor-linux-amd64"
+curl -sSfL -o hugoArticleEncryptor "https://github.com/hotjuicew/hugoArticleEncryptor/releases/download/stable/hugoArticleEncryptor-linux-amd64"
 
 chmod +x ./hugoArticleEncryptor
 
